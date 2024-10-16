c9864d0bf08bd0d6c74ff8ef026e860a73b4730e
JamesRtoP <yoilbob@outlook.com>
2024-10-16 12:43:55 -0700

    Place spirits in statics and working more on understanding routes

--- Backend/routes.py
+++ Backend/routes.py
@@ -10,10 +10,12 @@ from flask import url_for
 from flask import render_template
 #templates in flask via jinja2
 
-@app.route("/")#this creates a new page on the website
+
+#Old index page
+#@app.route("/")#this creates a new page on the website
 #if you go to the url/ it will bring you to this page
-def index():
-    return "<p>IDK what an index page is</p>"
+#def index():
+#    return "<p>IDK what an index page is</p>"
 
 
 @app.route("/kyan")#this creates a new page on the website
@@ -25,16 +27,78 @@ def kyan():
 def home():
     return '<h2> Welcome to Scrape The World </h2>'
 
+#Flask templates through jinja2 #thus the templates folder in backend
+@app.route('/hello')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', person=name)
+
+from flask import request
+
+@app.route("/user")
+def userBasePage():
+    login = "Login<br>"
+    newAccount = "Create New Account<br>"
+    return login + newAccount
+
+
+
+def sh_name(name):
+    oldName = name
+    name = 'Sh'
+    for x in range(len(oldName)):
+        if x != 0:
+            name += oldName[x]
+    return name
+
 #try not to end routes with a /, redirects to no slash, but adds complications
 @app.route("/user/<username>")
 def display_profile(username):
-    return f"User {escape(username)}"
+    Wassup = ("Wassup {}<br>")
+    user = f"User {escape(username)}<br>"
+    shuser = sh_name(str(username))+'<br>'
+    
+
+    return user + shuser + Wassup.format(username)
 
 
-#Flask templates through jinja2 #thus the templates folder in backend
-@app.route('/template/<t>')
-def tem(t):
-    return render_template('hello.html', person=t)
+@app.route("/")#this creates a new page on the website
+#if you go to the url/ it will bring you to this page
+def indexPage():
+    header = "<h1>Welcome To Scrape The World</h1>"
+    learningGoals = "<p1>The learning goals for this project are to learn how Flask works, then the parellel website thing, then eventually to learn webscraping </p1>"
+    link = '<p>Link to <a href="user">User</a> page</p>'#we got links now?
+    return header + learningGoals + link
+
+@app.latest("/late")
+def latest():
+    
+
+
+#everything below here is a little hellish
+
+#You will notice that code which depends on a request object will suddenly break because there is no request object. 
+#Whatever that means^
+#does whatever that means below
+with app.test_request_context('/hello', method='POST'):
+    # now you can do something with the request until the
+    # end of the with block, such as basic assertions:
+    assert request.path == '/hello'
+    assert request.method == 'POST'
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'],
+                       request.form['password']):
+            return log_the_user_in(request.form['username'])
+        else:
+            error = 'Invalid username/password'
+    # the code below is executed if the request method
+    # was GET or the credentials were invalid
+    return render_template('login.html', error=error)
+
 
 #print(url_for('home'))
 #should work but doesn't
@@ -71,3 +135,5 @@ def tem(t):
 #static files through python
 #url_for('static', filename = 'style.css')
 
+#<em> </em> italisize
+#<br> new line
