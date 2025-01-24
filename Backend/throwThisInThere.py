a219e70f3bdb8fb7ca4ede18c8f5f44fef177e2a
JamesRtoP <yoilbob@outlook.com>
2025-01-24 12:31:00 -0800

    getting into the weeds of cafe html

--- Backend/throwThisInThere.py
+++ Backend/throwThisInThere.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-from tqdm import tdqm_notebook
+#from tqdm import tdqm_notebook
 
 def save_html(html, path):
     with open(path, 'wb') as f:
@@ -12,21 +12,33 @@ def open_html(path):
     with open(path, 'rb') as f:
         return f.read()
 
-url = 'https://bellator.com/regular-fighter/magomed-magomedkerimov'
+url = 'https://dining.wsu.edu/southside-cafe'
 
 #r = requests.get(url)
-cont = open_html('figherP')
 #save_html(r.content, 'figherP')
+cont = open_html('figherP')
 
 soup = BeautifulSoup(cont, 'html.parser')
 
+#breakfast = soup.select('.accordion-item h3')
+breakfast = soup.select('#Breakfast h4, #Breakfast h3')
+#breakfast = soup.select('.accordion-item h3')
+
 #fighterPortrait = soup.select_one('.hero-profile__image')
-fighterPortrait = soup.select_one('img[src *= "bodyshots"]')['src']
+#fighterPortrait = soup.select_one('img[src *= "bodyshots"]')['src']
 #eventually I will remove 
-fighterName = soup.select_one('.container.text-shadow h1').text.strip()
+#fighterName = soup.select_one('.container.text-shadow h1').text.strip()
 #)
 
-print (fighterPortrait)
+
+#breakFastItems =  soup.select_one('#Breakfast h3')
+breakFastItems =  soup.find(id = 'Breakfast', )
+
+print (breakFastItems)
+#print (breakfast)
+#for i in breakFastItems:
+#    print(i)
+#    print("\n")
 
 #print(r.content[:100])
 #print("Yeah")
