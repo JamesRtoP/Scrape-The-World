2105352481b3cd8afcdaa270cfd53972148c3054
JamesRtoP <yoilbob@outlook.com>
2025-01-29 12:52:16 -0800

    derived and state runes

--- myapp/src/routes/+page.svelte
+++ myapp/src/routes/+page.svelte
@@ -1,32 +1,59 @@
 
-<svelte:options runes={true} />
 <script>
+
     
     let p =4;
     let was = "<strong>  wass up </strong>";
     let string = "need to html the next bit <strong> something</strong>" + {was};
 
+    let fives = $state([5])
+    function fiven()
+    {
+        fives.push(fives.at(0))
+    }
+    let fived = $derived(fives.reduce((t,n) => t*n,1))
+
+    let addy = $state([1,2,3])
+    function addyey()
+    {
+        addy.push(addy.length + 1)
+    }
+
     let count = $state(0);
+    let total = $derived(addy.reduce((t,n) => t + n, 0))
     function increment() {
-        count++
+        count++;
 		// TODO implement
 	}
 
 </script>
 
 
-<h1>Welcome to SvelteKit</h1>
-<p>{p} <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!--CSS Here-->
+<style>
+
+</style>
 
 {@html string} <!--@html tells it to double check for htmls-->
 <!--html does not sanatize string, so if someone else is entering it, then it could break everything: do escape yourself-->
 
-
+<br>
 <button onclick={increment}>
     Clicked {count}
     {count === 1 ? 'time' : 'times'}
 </button>
 
+<br>
+<button onclick={addyey}>
+    AddSome 
+</button>
+<p1> {addy.join ( ' + ')} = {total} </p1>
+
+<br>
+<button onclick={fiven}>
+    Five 
+</button>
+<p1> {fives.join ( ' * ')} = {fived} </p1>
 
 <!--
 <script>
