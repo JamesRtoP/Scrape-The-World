368917c01002911d68fc14e367885f77db6fd31b
JamesRtoP <yoilbob@outlook.com>
2025-05-06 10:37:52 -0700

    Using cafeString, it WORKS!!!

--- myapp/src/routes/+page.svelte
+++ myapp/src/routes/+page.svelte
@@ -2,17 +2,6 @@
 <script>
     
 
-    let was = "<strong>  wass up </strong>";
-    let string = "need to html the next bit <strong> something</strong>" + {was};
-
-    let fives = $state([5])
-    function fiven()
-    {
-        fives.push(fives.at(0))
-        console.log("ass")
-
-    }
-
     function fetchMeals()
     {
         fetch('./mealsToday.jason')
@@ -26,23 +15,7 @@
             .then(data=> console.log(data))
             .catch(error=> console.error('Failed to fetch data:', error))
     }
-    let fived = $derived(fives.reduce((t,n) => t*n,1))
 
-    let N = $state(0)
-    let oOfOneTotal = 2
-    let oOfLogN = $derived(Math.log(N)/Math.log(2))
-    let oOfNTotal = $derived(2*N)
-    let oOfNLogN = $derived(N*(Math.log(N)/Math.log(2)))
-    let oOfNSquaredTotal = $derived(N**2)
-    let oOfNPowTotal = $derived(2**N)
-
-
-    //console.log("butt")
-
-    function goingCrazy()
-    {
-        console.log("hello")
-    }
 
 
 
@@ -69,19 +42,28 @@
 
     }
     import mealsToday from '../mealsToday.json';
-    
-    //console.log(typeof mealsToday);
-    let x = 4;
-    //const southsideMenu = document.getElementsByClassName("cafe-menu-Southside");
+    Object.keys( mealsToday);
+
+    let JSONmeals = mealsToday
+    let entries = Object.entries(JSONmeals)
+    //cafe string will not be sanitized, so this is an issue
     let cafeString = "";
-    //let idk = mealsToday[station];
-    for (let station in Object.keys(mealsToday))
+    for (let meal in JSONmeals)
     {
-        cafeString += station;// += "<br>";
-        for (let something in mealsToday[station])
-        {
-            cafeString += something;
+        cafeString += `<div class = "food-station"> ${meal} </div>`;
+        for (let station in JSONmeals[meal])
+        {   
+            cafeString += "<br>Station: "+JSONmeals[meal][station]["station"] + "<br>";
+            for(let menuItemIndex in JSONmeals[meal][station]["menuItems"])
+            {
+                cafeString += JSONmeals[meal][station]["menuItems"][menuItemIndex][0]+ "<br>"
+            }
+            //cafeString += JSONmeals[meal][station]["menuItems"][0]+ "<br>";
         }
+        // for (let something in mealsToday[station])
+        // {
+        //     cafeString += something;
+        // }
     }
     //console.log(typeof mealsToday["Breakfast"]);
     //let something = document.getElementsByClassName("cafe-menu");
@@ -92,10 +74,7 @@
     //southsideMenu.innerHTML = cafeString;
     
 
-    Object.keys( mealsToday);
-
-    let JSONmeals = mealsToday
-    let entries = Object.entries(JSONmeals)
+    
     
 </script>
 
@@ -105,40 +84,13 @@
 -->
 
 
+{@html cafeString}
 
-
-{@html string} <!--@html tells it to double check for htmls-->
+<!-- {@html string} @html tells it to double check for htmls -->
 <!--html does not sanatize string, so if someone else is entering it, then it could break everything: do escape yourself-->
-
-<br>
-<button onclick={fiven}>
-    Five 
-</button>
-<p1> {fives.join ( ' * ')} = {fived} </p1>
-
-<br>
-<button onclick={N++}>
-N = {N}
-</button>
-<br>
-O(1) = {oOfOneTotal}
-<br>
-O(log N) = {oOfLogN}
-<br>
-O(N) = {oOfNTotal}
-<br>
-O(N log N) = {oOfNLogN}
-<br>
-O(N^2) = {oOfNSquaredTotal}
-<br>
-O(2^N) = {oOfNPowTotal}
-
-
-<br>
 
 <button onclick = {openAllDetails} type = "button"> Open </button>
 <button onclick = {closeAllDetails} type = "button"> Close </button>
-{openAllDetails()}
 
 
 <article class = c-wrapper>
@@ -168,27 +120,11 @@ O(2^N) = {oOfNPowTotal}
                 {#if i==0}
                     {mealthing}
                     <br>
-
-                        <!-- <script>
-                            let stations = Object.entries(mealthing)
-                            //let porkchop = 14
-                        </script>
-                        {stations} -->
                 {:else}
                     {#each mealthing as station, i}
-                        <!-- {station} -->
                         {#if i==1}
                         {JSON.stringify(station)}
                         {/if} 
-                        <!--
-                        <script>
-                            let foodItems =Object.entries(station);
-                        </script>
-                        {#each foodItems as foodItem}
-                            {foodItem}
-                        
-                        {/each}
-                        <br> -->
                     {/each}
                 {/if} 
             {/each}
@@ -198,20 +134,7 @@ O(2^N) = {oOfNPowTotal}
         {/each}        </div>
 
     </details>
-    {cafeString}
 </article>
-<!--
-<script>
-let p  = 4;
-p = 2;
-
-let src = './South Spirits.png';
-import Ghost from './South Spirits.png';
-</script>
-
-<img src = {Ghost} alt = "Three ghosts">
--->
-<!-- alt= "Three gravestones with ghosts behind them and the compass symbol S on the front grave"-->
 
 
 <!--CSS Here-->
@@ -245,6 +168,9 @@ import Ghost from './South Spirits.png';
         font-size:2vw;
 
     }
-
+    .food-station
+    {
+        font-size:1vh
+    }
     
 </style>
